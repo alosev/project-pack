ab3291a60f8441493dff5c473b8508406d05251f
Alexey Losev <lexa2201@mail.ru>
2017-01-23 14:21:54 +0300

    Загрузка проекта

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'); // сам gulp
+var sass = require('gulp-sass'); // sass
+var pug = require('gulp-pug'); // jade/pug
+var autoprefixer = require('gulp-autoprefixer'); // автопрефекс
+var spritesmith = require('gulp.spritesmith'); // спрайты
+var browserSync = require('browser-sync').create(); // локальный сервер + синхронизация браузера
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        port: 8080,
+        open: true,
+        notify: false
+    });
+});
+
+gulp.task('sass', function(){
+    gulp.src('src/sass/*.+(sass|scss)')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer({browsers: ['last 10 version', 'ie 9']}))
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('pug', function(){
+    gulp.src('src/jade/*.+(jade|pug)')
+        .pipe(pug({pretty: true}))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('src/images/sprite/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_variables.scss',
+            cssFormat: 'scss',
+            algorithm: 'binary-tree',
+            cssTemplate: 'custom.scss.handlebars',
+            cssVarMap: function(sprite) {
+                sprite.name = 's-' + sprite.name
+            }
+        }));
+    spriteData.img.pipe(gulp.dest('dist/images/'));
+    spriteData.css.pipe(gulp.dest('src/sass/sprite/'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/sass/**/*.+(sass|scss)', ['sass']); // Отслеживаем файлы sass
+    gulp.watch('src/jade/**/*.+(jade|pug)', ['pug']); // Отслеживаем файлы pug
+    gulp.watch('src/images/sprite/*.png', ['sprite']); // Отслеживаем файлы sprite/png
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
