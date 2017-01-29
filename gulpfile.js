efa828edf77bfea60c34c2d25a739b80b38b446d
Alexey Losev <lexa2201@mail.ru>
2017-01-30 01:40:00 +0300

    minify js, css

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,34 @@ var sass = require('gulp-sass'); // sass
 var pug = require('gulp-pug'); // jade/pug
 var autoprefixer = require('gulp-autoprefixer'); // автопрефекс
 var spritesmith = require('gulp.spritesmith'); // спрайты
-var browserSync = require('browser-sync').create(); // локальный сервер + синхронизация браузера
+var cleanCSS = require('gulp-clean-css'); // минификация css
+var uglify = require('gulp-uglify'); // минификация js
+//var browserSync = require('browser-sync').create(); // локальный сервер + синхронизация браузера
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist/"
-        },
-        port: 8080,
-        open: true,
-        notify: false
-    });
-});
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./dist/"
+//         },
+//         port: 8080,
+//         open: true,
+//         notify: false
+//     });
+// });
 
 gulp.task('sass', function(){
     gulp.src('src/sass/*.+(sass|scss)')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 10 version', 'ie 9']}))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(browserSync.stream())
+        .pipe(gulp.dest('src/css/'))
+        //.pipe(browserSync.stream())
 });
 
 gulp.task('pug', function(){
     gulp.src('src/jade/*.+(jade|pug)')
-        .pipe(pug({pretty: true}))
+        .pipe(pug({pretty: '\t'}))
         .pipe(gulp.dest('dist/'))
-        .pipe(browserSync.stream())
+        //.pipe(browserSync.stream())
 });
 
 gulp.task('sprite', function () {
@@ -43,14 +45,44 @@ gulp.task('sprite', function () {
                 sprite.name = 's-' + sprite.name
             }
         }));
-    spriteData.img.pipe(gulp.dest('dist/images/'));
+    spriteData.img.pipe(gulp.dest('src/images/'));
     spriteData.css.pipe(gulp.dest('src/sass/sprite/'));
+});
+
+gulp.task('css', function () {
+    gulp.src('src/css/*.css')
+        .pipe(gulp.dest('dist/css'))
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', function () {
+    gulp.src('src/js/*.js')
+        .pipe(gulp.dest('dist/js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('fonts', function () {
+    gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('images', function () {
+    gulp.src('src/images/*.+(png|jpg|jpeg|gif)')
+        .pipe(gulp.dest('dist/images'))
 });
 
 gulp.task('watch', function(){
     gulp.watch('src/sass/**/*.+(sass|scss)', ['sass']); // Отслеживаем файлы sass
     gulp.watch('src/jade/**/*.+(jade|pug)', ['pug']); // Отслеживаем файлы pug
+    gulp.watch('src/js/*.js', ['js']); // Отслеживаем файлы js
+    gulp.watch('src/css/*.css', ['css']); // Отслеживаем файлы css
+    gulp.watch('src/fonts/**/*', ['fonts']); // Отслеживаем файлы fonts
+    gulp.watch('src/images/*.+(png|jpg|jpeg|gif)', ['images']); // Отслеживаем файлы fonts
     gulp.watch('src/images/sprite/*.png', ['sprite']); // Отслеживаем файлы sprite/png
 });
 
-gulp.task('default', ['watch', 'browser-sync']);
+gulp.task('default', ['watch'/*, 'browser-sync']*/]);
